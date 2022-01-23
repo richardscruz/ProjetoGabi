3817e0245f7ed7a47dc4d3083a059a5114f7b2d0
unknown <richardscruz94@outlook.com>
2022-01-23 14:45:54 -0300

    Upload do site gabi

---
+++ script.js
@@ -0,0 +1,13 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+function carrossel(){
+    idx++;
+    if(idx > img.length -1){
+        idx = 0;
+}
+imgs.style.transform = `translateX(${-idx * 605}px)`;
+}
+setInterval(carrossel, 1000);
+
